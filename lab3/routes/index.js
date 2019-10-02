35034d33302c419bc7e20b5e69aabd851cd63247
shawn <blueshieldcross@gmail.com>
2019-10-02 19:24:46 -0400

    Add project files.

---
+++ lab3/routes/index.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+
+    var firstName = 'First Name: ';
+    var name;
+    var message;
+    if (req.query.name)
+        name = req.query.name;             //req.query.name -> ?name=shawn  / req.params.name -> /shawn
+
+        
+
+    res.render('index', {
+        title: 'List of Family Members: /Shawn/Sam/Sally/Rick/Hailey',
+        person: firstName + name + " " + message
+
+    }); 
+
+
+   
+
+});
+
+module.exports = router;
